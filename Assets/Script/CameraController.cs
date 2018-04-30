ed1435bddb2c25c10d7cfcf9c4eccbe4f0d75fb0
heartunderblade85817155 <xiewenzhao85817155@163.com>
2018-04-30 13:01:02 +0800

    2018.4.30 资源

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera MainCamera;
+
+    public float CameraZSpeed = 0.5f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (MainCamera)
+        {
+            Vector3 CameraCurrentPosition = MainCamera.transform.position;
+            MainCamera.transform.position = new Vector3(CameraCurrentPosition.x, CameraCurrentPosition.y, CameraCurrentPosition.z - CameraZSpeed * Time.deltaTime);
+        }
+	}
+}
